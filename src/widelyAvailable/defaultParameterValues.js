14106e85724c6140172c9e299ba5a9fb950f8c2b
Johannes Maron <johannes@maron.family>
2026-01-10 15:56:11 +0100

    Move code

--- src/widelyAvailable/defaultParameterValues.js
+++ src/widelyAvailable/defaultParameterValues.js
@@ -1,5 +1,4 @@
 import { default as j } from "jscodeshift"
-import { findEnclosingFunction, NodeTest } from "../types.js"
 
 /**
  * Transform manual default value assignment to default parameters.
@@ -160,77 +159,4 @@ export function defaultParameterValues(root) {
   })
 
   return modified
-}
-
-/**
- * Unwrap Promise.resolve() and Promise.reject() calls.
- * - Promise.resolve(value) -> value
- * - Promise.reject(error) -> marker object { _isReject: true, argument: error } for special handling
- *
- * @param {*} node - The AST node to potentially unwrap
- * @returns {*} The unwrapped value, a reject marker object, or the original node
- */
-function unwrapPromiseResolveReject(node) {
-  if (
-    j.CallExpression.check(node) &&
-    j.MemberExpression.check(node.callee) &&
-    j.Identifier.check(node.callee.object) &&
-    node.callee.object.name === "Promise" &&
-    j.Identifier.check(node.callee.property)
-  ) {
-    if (node.callee.property.name === "resolve") {
-      return node.arguments.length === 1 ? node.arguments[0] : j.identifier("undefined")
-    }
-    if (node.callee.property.name === "reject") {
-      return {
-        _isReject: true,
-        argument:
-          node.arguments.length === 1 ? node.arguments[0] : j.identifier("undefined"),
-      }
-    }
-  }
-  return node
-}
-
-/**
- * Make a function async and transform its promise returns.
- *
- * @param {import("jscodeshift").NodePath} funcPath - The function path
- * @param {Function} callback - Callback to transform return paths
- * @returns {boolean} True if the function was modified
- */
-function _transformFunctionToAsync(funcPath, callback) {
-  const func = funcPath.node
-
-  if (func.async || !j.BlockStatement.check(func.body)) {
-    return false
-  }
-
-  let hasPromiseReturn = false
-  const promiseReturns = []
-
-  j(funcPath)
-    .find(j.ReturnStatement)
-    .forEach((retPath) => {
-      const enclosing = findEnclosingFunction(retPath)
-      if (enclosing !== funcPath) {
-        return
-      }
-
-      if (
-        retPath.node.argument &&
-        new NodeTest(retPath.node.argument).isKnownPromise()
-      ) {
-        hasPromiseReturn = true
-        promiseReturns.push(retPath)
-      }
-    })
-
-  if (!hasPromiseReturn) {
-    return false
-  }
-
-  func.async = true
-  callback(promiseReturns)
-  return true
 }
